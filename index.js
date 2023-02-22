f32dedce83f415f386fabded4548d4e5a0631392
shelby <shelbynarnold@yahoo.com>
2023-02-22 16:48:48 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(symbol){
+
+    if(symbol == "||"){
+        return function result(){
+            return `You are ${symbol}a dedicated programmer${symbol}!`
+        }
+    } else {
+        if (symbol == '*'){
+            return function result(){
+            return `You are ${symbol}a hard worker${symbol}!`
+        }}
+    }
+}
